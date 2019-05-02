97c3816a928eb53aba91823b2b41df0c9a933991
iomi <omidi.iman@gmail.com>
2019-05-02 10:13:23 +0430

    Finishing  Touches for github (not complete)

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -27,7 +27,7 @@ export default Cart;
 
 const StCart = styled.div`
   position: fixed;
-  top: 0;
+  bottom: 0;
   left: 0;
   width: 100%;
   background: white;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 const Header = () => {
-  return <StHeader>Heloo</StHeader>;
+  return (
+    <StHeader>
+      <h1>PokeApp</h1>
+    </StHeader>
+  );
 };
 
 export default Header;
 
 const StHeader = styled.header`
-  height: 40px;
+  text-align: center;
+  padding: 0.2em;
   background-color: #bada55;
 `;
